d56ecba717da32c269fefbd9b783f8ad115900ae
KevinPurwanto <kevinpurwanto2004@gmail.com>
2025-08-14 13:33:23 +0700

    Testdeploy

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta name="username" content="{{ Auth::user()->name }}">
+        <meta name="username" content="{{ Auth::check() ? Auth::user()->name : '' }}">
+
 
 
         {{-- Inline script to detect system dark mode preference and apply it immediately --}}
